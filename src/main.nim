d879f6062c67ba1e4ef6ff37d1fd7f40aecdf92d
VaguinerG <vagfilmes@gmail.com>
2025-07-24 18:01:28 -0300

    Refactor build commands for Linux compilation and remove unused functions

--- src/main.nim
+++ src/main.nim
@@ -3,10 +3,14 @@ import raylib # core graphics library
 
 include internal/[core/constants, core/types, core/globals]
 
+
 setTraceLogLevel(None)
 setConfigFlags(flags(WindowResizable, Msaa4xHint, VsyncHint))
 
 initWindow(BASE_WINDOW_HEIGHT, BASE_WINDOW_WIDTH, "Dark Rogue")
+
+proc drawScene(_: Logos.type) =
+    echo true
 
 block:
     while not windowShouldClose():
